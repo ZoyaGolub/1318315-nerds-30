ff4e5b1138c14483a3bc1f8a17dc74eea73ba4a0
ZoyaGolub <zjgolub@gmail.com>
2021-03-09 11:21:17 +0200

    Прописала модальное окно в JS

---
+++ js/modal-letter.js
@@ -0,0 +1,66 @@
+const modalLetter = document.querySelector(".footer-contacts-button");
+const modalPopup = document.querySelector(".modal");
+const modalClose = modalPopup.querySelector(".modal-close");
+const modalForm = modalPopup.querySelector(".modal-info");
+const modalName = modalPopup.querySelector(".modal-info-text-name");
+const modalEmail = modalPopup.querySelector(".modal-info-text-email");
+const modalText = modalPopup.querySelector(".modal-info-text-letter");
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+    storage = localStorage.getItem("name");
+    storage = localStorage.getItem("email");
+} catch (err) {
+    isStorageSupport = false;
+}
+
+footer-contacts-button.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalPopup.classList.add("modal-show");
+
+    if (storage) {
+        modalName.value = storage;
+        modalEmail.focus();
+    }   else {
+        modalName.focus();
+        if (storage) {
+            modalEmail.value = storage;
+            modalText.focus();
+        }   else {
+            modalEmail.focus();
+        }
+    }
+
+    modalName.focus();
+});
+
+modalClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalPopup.classList.remove("modal-show");
+    modalPopup.classList.remove("modal-error");
+});
+
+modalForm.addEventListener("submit", function (evt) {
+    if (!modalName.value || !modalEmail.value || !modalText.value) {
+        evt.preventDefault();
+        modalPopup.classList.remove("modal-error");
+        modalPopup.offsetWidth = modalPopup.offsetWidth;
+        modalPopup.classList.add("modal-error");
+    }   else {
+        if (isStorageSupport) {
+            localStorage.setItem("name", modalName.value);
+            localStorage.setItem("email", modalEmail.value);
+      }
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        if (modalPopup.classList.contains("moodal-show")) {
+            evt.preventDefault();
+            modalPopup.classList.remove("modal-show");
+        }
+    }
+});
