38a839f4d3f4333bb500e4d17b9d6445518a9828
ZoyaGolub <zjgolub@gmail.com>
2021-03-19 15:57:35 +0200

    Внесение правок после первой проверки
    Кнопку вызывающую попап сделала ссылкой
    Исправила ошибку в JS
    Исправила аниммацию
    прописала слайды и карточки из скрытых слоев
    Исправила использование цветов
    Вставила гугл-карту
    Отформатировала код в css и js
    Минимизировала код в css и js

--- js/modal-letter.js
+++ js/modal-letter.js
@@ -10,57 +10,58 @@ let isStorageSupport = true;
 let storage = "";
 
 try {
-    storage = localStorage.getItem("name");
-    storage = localStorage.getItem("email");
+  storage = localStorage.getItem("name");
+  storage = localStorage.getItem("email");
 } catch (err) {
-    isStorageSupport = false;
+  isStorageSupport = false;
 }
 
 modalLetter.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    modalPopup.classList.add("modal-show");
+  evt.preventDefault();
+  modalPopup.classList.add("modal-show");
 
-    if (storage) {
-        modalName.value = storage;
-        modalEmail.focus();
-    }   else {
-        modalName.focus();
-        if (storage) {
-            modalEmail.value = storage;
-            modalText.focus();
-        }   else {
-            modalEmail.focus();
-        }
-    }
-
+  if (storage) {
+    modalName.value = storage;
+    modalEmail.focus();
+  } else {
     modalName.focus();
+    if (storage) {
+      modalEmail.value = storage;
+      modalText.focus();
+    } else {
+      modalEmail.focus();
+    }
+  }
+
+  modalName.focus();
 });
 
 modalClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    modalPopup.classList.remove("modal-show");
-    modalPopup.classList.remove("modal-error");
+  evt.preventDefault();
+  modalPopup.classList.remove("modal-show");
+  modalPopup.classList.remove("modal-error");
 });
 
 modalForm.addEventListener("submit", function (evt) {
-    if (!modalName.value || !modalEmail.value || !modalText.value) {
-        evt.preventDefault();
-        modalPopup.classList.remove("modal-error");
-        modalPopup.offsetWidth = modalPopup.offsetWidth;
-        modalPopup.classList.add("modal-error");
-    }   else {
-        if (isStorageSupport) {
-            localStorage.setItem("name", modalName.value);
-            localStorage.setItem("email", modalEmail.value);
-      }
+  if (!modalName.value || !modalEmail.value || !modalText.value) {
+    evt.preventDefault();
+    modalPopup.classList.remove("modal-error");
+    modalPopup.offsetWidth = modalPopup.offsetWidth;
+    modalPopup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", modalName.value);
+      localStorage.setItem("email", modalEmail.value);
     }
+  }
 });
 
 window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-        if (modalPopup.classList.contains("moodal-show")) {
-            evt.preventDefault();
-            modalPopup.classList.remove("modal-show");
-        }
+  if (evt.keyCode === 27) {
+    if (modalPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      modalPopup.classList.remove("modal-show");
+      modalPopup.classList.remove("modal-error");
     }
+  }
 });
